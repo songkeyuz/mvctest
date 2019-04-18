52e2738d59269e903483869deaa3be732955db94
songkeyuz <1158300702@qq.com>
2019-04-18 23:05:42 +0800

    完善功能

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,27 +3,53 @@
 
 // Write your JavaScript code.
 
-    window.onload = function (ev) {
-
-    var oBtn = document.getElementById("button");
-
-    oBtn.onclick = function (ev1) {
-        var user = {
-            username:$("#UserName").val(),
-            password:$("#Password1").val()
+window.onload = function () {
+    $("#userLogIn").click(function () {
+        login()
+    })
+    $("#logIn").keydown(function (e) {
+        if (e.keyCode == 13) {
+            login();
         }
+    })
+    function login() {
+        var user = {
+            username: $("#UserName").val(),
+            password: $("#Password1").val(),
+        }
+
+        if ($.trim($("#UserName").val()).length == 0) {
+            $("#user").removeClass("hint");
+            setTimeout(function () {
+                $("#user").addClass("hint");
+            }, 1500);
+            return false;
+        };
+        if ($.trim($("#Password1").val()).length == 0) {
+            $("#pwd").removeClass("hint");
+            setTimeout(function () {
+                $("#pwd").addClass("hint");
+            }, 1500);
+            return false;
+        };
         $.ajax({
             type: "post",
             url: "/account/dologin",
             data: user,
             dataType: "json",
-            success: function (response,config) {
-                alert(response.responseTxet);
+            success: function (res) {
+                if (res.code == 200) {
+                    alert("跳转中");
+                    // window.location.href = "www.baidu.com"
+                }
+                if (res.code == 400) {
+                    alert(res.message);
+                }
             },
-            error: function(){
-                alert("用户名或密码错误");
+            error: function () {
+                alert("未访问到服务器");
             }
-            
+
         });
     }
 }
